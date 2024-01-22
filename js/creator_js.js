b75e40cdbfea833f60e3fb9bc23e262b28786985
shivanand <shivanandchoudhary22@gmail.com>
2024-01-22 17:08:54 +0530

    Optimization

---
+++ js/creator_js.js
@@ -0,0 +1,241 @@
+const creatorsData = [
+  {
+    name: "SHRADHEY PATHAK",
+    branch: "IT",
+    rollNumber: "20198111",
+    linkedin: "https://www.linkedin.com/in/shradhey-pathak",
+    image: "./images/creators/Shradhey Pathak.jpg",
+  },
+  {
+    name: "Sreemoyee Basu",
+    branch: "ECE",
+    rollNumber: "20195100",
+    linkedin: "#", // Replace with the actual LinkedIn link
+    image:
+      "./images/creators/E35E7195-2375-40E9-BD52-E89433333705 - Sreemoyee Basu.jpeg",
+  },
+  {
+    name: "Ayush Aman",
+    branch: "CIVIL",
+    rollNumber: "20193222",
+    linkedin: "https://www.linkedin.com/mwlite/in/ayush-aman",
+    image: "./images/creators/IMG_20221207_223353 - Ayush Aman.jpg",
+  },
+  // Add other creators with similar structure
+  {
+    name: "Shatrunjay Srivastava",
+    branch: "CIVIL",
+    rollNumber: "20191009",
+    linkedin: "https://www.linkedin.com/in/shatrunjay-srivastava",
+    image: "./images/creators/shatru - Shatrunjay Srivastava.jpeg",
+  },
+  {
+    name: "Anoop Bhatnagar",
+    branch: "Chemical",
+    rollNumber: "20199045",
+    linkedin: "https://www.linkedin.com/in/anoop1611",
+    image: "./images/creators/AnoopBhatnagar - anoop bhatnagar.jpg",
+  },
+  {
+    name: "Animesh Ranjan",
+    branch: "CHEMICAL",
+    rollNumber: "20199005",
+    linkedin: "#", // Replace with the actual LinkedIn link
+    image: "./images/creators/IMG_20230223_134120 - Animesh Ranjan (ANI).jpg",
+  },
+  {
+    name: "GUNGUN AGRAWAL",
+    branch: "BIOTECH",
+    rollNumber: "20190047",
+    linkedin: "https://www.linkedin.com/in/gungun-agrawal",
+    image: "./images/creators/IMG-20221102-WA0004(1) - Gungun Agrawal.jpg",
+  },
+  {
+    name: "Rahul Verma",
+    branch: "PIE",
+    rollNumber: "20190042",
+    linkedin: "https://www.linkedin.com/in/rahulvmnnit",
+    image:
+      "./images/creators/WhatsApp Image 2023-02-23 at 1.42.12 PM - Rahul Verma.jpeg",
+  },
+  {
+    name: "Vaishnavi Tripathi",
+    branch: "CSE",
+    rollNumber: "20204221",
+    linkedin: "https://www.linkedin.com/in/vaishnavi-tripathi-888350207",
+    image: "./images/creators/IMG-20221212-WA0041 - Vaishnavi Tripathi.jpg",
+  },
+  {
+    name: "Priyanshu Agarwal",
+    branch: "CSE",
+    rollNumber: "20204152",
+    linkedin: "#", // Replace with the actual LinkedIn link
+    image:
+      "./images/creators/WhatsApp Image 2023-02-22 at 21.42.45 - Priyanshu Agarwal.jpeg",
+  },
+  {
+    name: "Jiya Chauhan",
+    branch: "IT",
+    rollNumber: "20208061",
+    linkedin: "#", // Replace with the actual LinkedIn link
+    image: "./images/creators/DSC_0344 - Jiya Chauhan.jpg",
+  },
+  {
+    name: "KISHLAY KASHISH",
+    branch: "IT",
+    rollNumber: "20208067",
+    linkedin: "https://www.linkedin.com/in/kishlay-kashish-02032a20a",
+    image: "./images/creators/kishlay - Kishlay Kashish.JPG",
+  },
+  {
+    name: "Mradul Garg",
+    branch: "ECE",
+    rollNumber: "20205100",
+    linkedin: "https://www.linkedin.com/in/gargmradul",
+    image:
+      "./images/creators/Screenshot_20230222_112852_Photos - Mradul Garg.jpg",
+  },
+  {
+    name: "Akshat Agarwal",
+    branch: "ECE",
+    rollNumber: "20205021",
+    linkedin: "www.linkedin.com/in/aka141220", // Fix the link format
+    image:
+      "./images/creators/WhatsApp Image 2023-02-22 at 1.00.17 AM - Akshat Agarwal.jpeg",
+  },
+    {
+      name: "Alankar Bansal",
+      branch: "ECE",
+      rollNumber: "20205024",
+      linkedin: "#", // Replace with the actual LinkedIn link
+      image: "./images/creators/DSC_6473 - Alankar Bansal.JPG",
+    },
+    {
+      name: "Dolly Agrawal",
+      branch: "Mechanical",
+      rollNumber: "20203053",
+      linkedin: "https://www.linkedin.com/in/dolly-agrawal",
+      image: "./images/creators/WhatsApp Image 2023-02-22 at 11.49.15 PM - Dolly Agrawal.jpeg",
+    },
+    {
+      name: "Anushka Chauhan",
+      branch: "Mechanical",
+      rollNumber: "20203029",
+      linkedin: "https://www.linkedin.com/in/anushka-chauhan-a0b43620a",
+      image: "./images/creators/IMG_20220801_104034_886[1] - Anushka Chauhan.jpg",
+    },
+    {
+      name: "Aitha Sai Rishik",
+      branch: "Electrical",
+      rollNumber: "20202009",
+      linkedin: "#", // Replace with the actual LinkedIn link
+      image: "./images/creators/aitha.jpeg",
+    },
+    {
+      name: "Mohd Taha",
+      branch: "Civil",
+      rollNumber: "20201066",
+      linkedin: "https://www.linkedin.com/in/mohd-taha-mnnit",
+      image: "./images/creators/20230218_212223 - Mohd Taha.jpg",
+    },
+    {
+      name: "Piyush Kashyap",
+      branch: "Civil",
+      rollNumber: "20201075",
+      linkedin: "https://www.linkedin.com/in/piyush-kashyap-a72229203",
+      image: "./images/creators/image - Piyush Kashyap.jpeg",
+    },
+    {
+      name: "Aman Sharma",
+      branch: "Civil",
+      rollNumber: "20201016",
+      linkedin: "https://www.linkedin.com/in/aman-sharma-99a68920a",
+      image: "./images/creators/IMG_20230207_115907_617 - Aman Sharma.jpg",
+    },
+    {
+      name: "Aditya Baranwal",
+      branch: "Civil",
+      rollNumber: "20201010",
+      linkedin: "https://in.linkedin.com/in/aditya-baranwal-013",
+      image: "./images/creators/DSCN1712 - Aditya Baranwal.JPG",
+    },
+    {
+      name: "Ratna Rathaur",
+      branch: "Biotech",
+      rollNumber: "20200041",
+      linkedin: "http://www.linkedin.com/in/ratna-rathaur",
+      image: "./images/creators/IMG_20220602_193643009_PORTRAIT - Ratna Rathaur.jpg",
+    },
+    {
+      name: "Afzal Ansari",
+      branch: "Biotech",
+      rollNumber: "20200007",
+      linkedin: "https://www.linkedin.com/in/afzalansari19",
+      image: "./images/creators/IMG_20230222_002956 - AFZAL ANSARI.jpg",
+    },
+    {
+      name: "Himanshu Gautam",
+      branch: "Chemical",
+      rollNumber: "20209027",
+      linkedin: "#", // Replace with the actual LinkedIn link
+      image: "./images/creators/IMG_20221228_160839_241 - Himanshu gautam.jpg",
+    },
+    {
+      name: "Vishwajeet Kumar",
+      branch: "Chemical",
+      rollNumber: "20209060",
+      linkedin: "https://www.linkedin.com/in/vishwa511",
+      image: "./images/creators/me - vishwajeet kumar.jpg",
+    },
+    {
+      name: "Aman Kumar",
+      branch: "Chemical",
+      rollNumber: "20209004",
+      linkedin: "#", // Replace with the actual LinkedIn link
+      image: "./images/creators/aman kumar.jpeg",
+    },
+    {
+      name: "Adarsh Pandey",
+      branch: "PIE",
+      rollNumber: "20206002",
+      linkedin: "https://www.linkedin.com/in/adarshpandey951",
+      image: "./images/creators/IMG-20230222-WA0000 - Adarsh Pandey.jpg",
+    },
+    {
+      name: "Shashwat Gupta",
+      branch: "PIE",
+      rollNumber: "20206045",
+      linkedin: "https://www.linkedin.com/in/shashwat13g/",
+      image: "./images/creators/IMG-20230210-WA0020 - Shashwat Gupta.jpg",
+    }
+];
+
+// Get the container to append creator cards
+const creatorsContainer = document.getElementById("container11");
+
+// Function to create creator cards dynamically
+function createCreatorCard(creator) {
+  return `
+        <div class="card">
+            <div class="content">
+                <div class="imgBx">
+                    <img src="${creator.image}">
+                </div>
+                <div class="contentBx">
+                    <h3>${creator.name} <br> <span>${creator.branch} <br> ${creator.rollNumber}</span></h3>
+                </div>
+            </div>
+            <div class="sci">
+                <li>
+                    <a href="${creator.linkedin}" class="fa fa-linkedin"></a>
+                </li>
+            </div>
+        </div>
+    `;
+}
+
+// Loop through the creators and append cards to the container
+creatorsData.forEach((creator) => {
+  const creatorCard = createCreatorCard(creator);
+  creatorsContainer.innerHTML += creatorCard;
+});
---
+++ js/home_js.js
@@ -0,0 +1,131 @@
+// <!-- Department Section -->
+// Array of departments with their details
+const departmentDetails = [
+  {
+    name: "First Year",
+    image: "./images/first.jpg",
+    description: "Baitho beta abhi bahot kuch seekhna baaki hai Jeevan me..",
+    link1: "http://paperfactorymnnit.pythonanywhere.com/",
+    link2:
+      "https://drive.google.com/drive/folders/1-2Ecfzlgj5Kr260HKx1B8pgCDtWbiDhF",
+  },
+  {
+    name: "Computer Science and Engineering",
+    image: "./images/cse (4).jpg",
+    description:
+      "Abundance of DSA on the internet, now abundance of academic resources on this Page.",
+    link1:
+      "https://drive.google.com/drive/u/1/folders/1MLMiaW0vq0K0lWtUakqLNXjBIfSa9Nw0",
+  },
+  {
+    name: "Electronics & Communication Engineering",
+    image: "./images/ece.jpg",
+    description:
+      "Hardwork amplified and concepts doped. And this leads to saturation of success.",
+    link1:
+      "https://drive.google.com/drive/folders/16g1SvodQLuXZknBlNXjVJXmBMUkcmZ8V",
+  },
+  {
+    name: "Electrical Engineering",
+    image: "./images/electrical.jpg",
+    description:
+      "Electrifying the academics with least resistance and conducting knowledge through wires of SMP Resources..",
+    link1:
+      "https://drive.google.com/drive/u/4/folders/1qI6i_v7dTvWirq8syp60VAo750c-xXlH",
+  },
+  {
+    name: "Civil Engineering",
+    image: "./images/civil.jpg",
+    description:
+      "Survey fields, not the internet for notes. Here's your one stop destination for all civil resources-SMP Resource Hub",
+    link1:
+      "https://drive.google.com/drive/folders/1Ly4pGcHli_UU1XmLgtbT6qE_EV43LYjY",
+  },
+  {
+    name: "Mechanical Engineering",
+    image: "./images/mech.jpg",
+    description:
+      "Use the SMP Resource Hub to study the inner-workings of our world with less power and maximum efficiency",
+    link1:
+      "https://drive.google.com/drive/folders/12Bv_TftkQwZtE7ho9Zzm0UXKbYx1CBeg",
+  },
+  {
+    name: "Chemical Engineering",
+    image: "./images/chemical.jpg",
+    description:
+      "Hard time finding the notes? We have the 'solution' to keep you in 'equilibrium'.",
+    link1:
+      "https://drive.google.com/drive/folders/1LS5fXhC0G5ykhd8-yl_WD4Cjj-mC6VzT?usp=share_link",
+  },
+  {
+    name: "Biotechnology",
+    image: "./images/biotech.jpg",
+    description:
+      "Resources so concise and complete that it would never 'cell' you out..",
+    link1:
+      "https://drive.google.com/drive/u/3/folders/10Gl296BcmzT2v8oaKSQQsBthFT8USjex",
+  },
+  {
+    name: "Production & Industrial Engineering",
+    image: "./images/pie.jpg",
+    description:
+      "Use the SMP Resource Hub to study the inner-workings of our world with less power and maximum efficiency",
+    link1:
+      "https://drive.google.com/drive/folders/1THq-Nv3vxPxsDle_HhvKeEBmroz8LWDi?usp=share_link",
+  },
+  {
+    name: "Final Year Open Electives",
+    image: "./images/final.jpg",
+    description: "Finally you came to this.. Let's end this sem with a Bang !!",
+    link1: "#",
+  },
+  {
+    name: "Internship & Placement Preparation",
+    image: "./images/intern.jpg",
+    description:
+      "Naukari ke bina kuch nahi hai Beta !! Abhi bhi time hai padh lo..",
+    link1: "#",
+  },
+];
+
+// Function to create department cards dynamically
+function createDepartmentCard(department) {
+  // Check the department name and set links accordingly
+  let linksContent;
+  if (department.name === "First Year") {
+    linksContent = `
+            <a href="${department.link1}" class="btn btn-primary">Click here (Paper Factory)</a> <br>
+            <a href="${department.link2}" class="btn btn-primary">Click here (New)</a>
+        `;
+  } else {
+    linksContent = `
+            <a href="${department.link1}" class="btn btn-primary">Click here</a> <br>
+        `;
+  }
+
+  return `
+        <div class="card" data-aos="fade-right" style="transition: all 1.5s;">
+            <div class="face face1">
+                <div class="content">
+                    <img src="${department.image}" height="120px">
+                    <h3>${department.name}</h3>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content">
+                    <p>${department.description}</p>
+                    ${linksContent}
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+// Get the container to append department cards
+const departmentContainer = document.getElementById("department-container");
+
+// Loop through the departments and append cards to the container
+departmentDetails.forEach((department) => {
+  const departmentCard = createDepartmentCard(department);
+  departmentContainer.innerHTML += departmentCard;
+});
