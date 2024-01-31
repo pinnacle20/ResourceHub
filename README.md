ca6b2553fec24d8c08e046fc3725da0106fd74a5
shivanand <shivanandchoudhary22@gmail.com>
2024-01-31 18:11:54 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
 # SMP
 
-No good understanding can ever be brought without the skillful notes; thus, SMP contributed as an open-source platform for archiving one of the best notes and books for all the students of MNNIT. 
+No good understanding can ever be brought without the skillful notes; thus, SMP contributed as an open-source platform for archiving one of the best notes and books for all the students of MNNIT.
 
 Resource Hub aimed at providing notes to all for depth in understanding and bringing transparency in the concepts.
 
 Here is the new version of SMP Resource Hub, which is better, updated and more sorted.
+
+## Contributors Guideline
+
+#### Step 0:
+
+Fork the repository.
+
+#### Step 1:
+
+Add your image to images/creators.
+
+#### Step 2:
+
+Create an object:
+
+"{
+
+name: "Name",
+
+branch: "Branch",
+
+rollNumber: "RegistrationNumber",
+
+linkedin: "ProfileLink",
+
+image: "./images/creators/{nameOfImageFile}",
+
+}"
+
+#### Step 3:
+
+Add the created object to the contributorsData array present in the creator_js.js file.
+
+#### Step 4:
+
+Make a Pull Request.
--- js/creator_js.js
+++ js/creator_js.js
@@ -12,7 +12,7 @@ const creatorsData = [
     rollNumber: "20208118",
     linkedin: "https://www.linkedin.com/in/shivanand07/",
     image: "./images/creators/Shivanand.jpg",
-  }
+  },
 ];
 
 const contributorsData = [
@@ -27,7 +27,7 @@ const contributorsData = [
     name: "Sreemoyee Basu",
     branch: "ECE",
     rollNumber: "20195100",
-    linkedin: "#", // Replace with the actual LinkedIn link
+    linkedin: "#",
     image:
       "./images/creators/E35E7195-2375-40E9-BD52-E89433333705 - Sreemoyee Basu.jpeg",
   },
@@ -38,7 +38,6 @@ const contributorsData = [
     linkedin: "https://www.linkedin.com/mwlite/in/ayush-aman",
     image: "./images/creators/IMG_20221207_223353 - Ayush Aman.jpg",
   },
-  // Add other creators with similar structure
   {
     name: "Shatrunjay Srivastava",
     branch: "CIVIL",
@@ -57,7 +56,7 @@ const contributorsData = [
     name: "Animesh Ranjan",
     branch: "CHEMICAL",
     rollNumber: "20199005",
-    linkedin: "#", // Replace with the actual LinkedIn link
+    linkedin: "#",
     image: "./images/creators/IMG_20230223_134120 - Animesh Ranjan (ANI).jpg",
   },
   {
@@ -86,7 +85,7 @@ const contributorsData = [
     name: "Priyanshu Agarwal",
     branch: "CSE",
     rollNumber: "20204152",
-    linkedin: "#", // Replace with the actual LinkedIn link
+    linkedin: "#",
     image:
       "./images/creators/WhatsApp Image 2023-02-22 at 21.42.45 - Priyanshu Agarwal.jpeg",
   },
@@ -94,7 +93,7 @@ const contributorsData = [
     name: "Jiya Chauhan",
     branch: "IT",
     rollNumber: "20208061",
-    linkedin: "#", // Replace with the actual LinkedIn link
+    linkedin: "#",
     image: "./images/creators/DSC_0344 - Jiya Chauhan.jpg",
   },
   {
@@ -116,115 +115,118 @@ const contributorsData = [
     name: "Akshat Agarwal",
     branch: "ECE",
     rollNumber: "20205021",
-    linkedin: "www.linkedin.com/in/aka141220", // Fix the link format
+    linkedin: "https://www.linkedin.com/in/aka141220",
     image:
       "./images/creators/WhatsApp Image 2023-02-22 at 1.00.17 AM - Akshat Agarwal.jpeg",
   },
-    {
-      name: "Alankar Bansal",
-      branch: "ECE",
-      rollNumber: "20205024",
-      linkedin: "#", // Replace with the actual LinkedIn link
-      image: "./images/creators/DSC_6473 - Alankar Bansal.JPG",
-    },
-    {
-      name: "Dolly Agrawal",
-      branch: "Mechanical",
-      rollNumber: "20203053",
-      linkedin: "https://www.linkedin.com/in/dolly-agrawal",
-      image: "./images/creators/WhatsApp Image 2023-02-22 at 11.49.15 PM - Dolly Agrawal.jpeg",
-    },
-    {
-      name: "Anushka Chauhan",
-      branch: "Mechanical",
-      rollNumber: "20203029",
-      linkedin: "https://www.linkedin.com/in/anushka-chauhan-a0b43620a",
-      image: "./images/creators/IMG_20220801_104034_886[1] - Anushka Chauhan.jpg",
-    },
-    {
-      name: "Aitha Sai Rishik",
-      branch: "Electrical",
-      rollNumber: "20202009",
-      linkedin: "#", // Replace with the actual LinkedIn link
-      image: "./images/creators/aitha.jpeg",
-    },
-    {
-      name: "Mohd Taha",
-      branch: "Civil",
-      rollNumber: "20201066",
-      linkedin: "https://www.linkedin.com/in/mohd-taha-mnnit",
-      image: "./images/creators/20230218_212223 - Mohd Taha.jpg",
-    },
-    {
-      name: "Piyush Kashyap",
-      branch: "Civil",
-      rollNumber: "20201075",
-      linkedin: "https://www.linkedin.com/in/piyush-kashyap-a72229203",
-      image: "./images/creators/image - Piyush Kashyap.jpeg",
-    },
-    {
-      name: "Aman Sharma",
-      branch: "Civil",
-      rollNumber: "20201016",
-      linkedin: "https://www.linkedin.com/in/aman-sharma-99a68920a",
-      image: "./images/creators/IMG_20230207_115907_617 - Aman Sharma.jpg",
-    },
-    {
-      name: "Aditya Baranwal",
-      branch: "Civil",
-      rollNumber: "20201010",
-      linkedin: "https://in.linkedin.com/in/aditya-baranwal-013",
-      image: "./images/creators/DSCN1712 - Aditya Baranwal.JPG",
-    },
-    {
-      name: "Ratna Rathaur",
-      branch: "Biotech",
-      rollNumber: "20200041",
-      linkedin: "http://www.linkedin.com/in/ratna-rathaur",
-      image: "./images/creators/IMG_20220602_193643009_PORTRAIT - Ratna Rathaur.jpg",
-    },
-    {
-      name: "Afzal Ansari",
-      branch: "Biotech",
-      rollNumber: "20200007",
-      linkedin: "https://www.linkedin.com/in/afzalansari19",
-      image: "./images/creators/IMG_20230222_002956 - AFZAL ANSARI.jpg",
-    },
-    {
-      name: "Himanshu Gautam",
-      branch: "Chemical",
-      rollNumber: "20209027",
-      linkedin: "#", // Replace with the actual LinkedIn link
-      image: "./images/creators/IMG_20221228_160839_241 - Himanshu gautam.jpg",
-    },
-    {
-      name: "Vishwajeet Kumar",
-      branch: "Chemical",
-      rollNumber: "20209060",
-      linkedin: "https://www.linkedin.com/in/vishwa511",
-      image: "./images/creators/me - vishwajeet kumar.jpg",
-    },
-    {
-      name: "Aman Kumar",
-      branch: "Chemical",
-      rollNumber: "20209004",
-      linkedin: "#", // Replace with the actual LinkedIn link
-      image: "./images/creators/aman kumar.jpeg",
-    },
-    {
-      name: "Adarsh Pandey",
-      branch: "PIE",
-      rollNumber: "20206002",
-      linkedin: "https://www.linkedin.com/in/adarshpandey951",
-      image: "./images/creators/IMG-20230222-WA0000 - Adarsh Pandey.jpg",
-    },
-    {
-      name: "Shashwat Gupta",
-      branch: "PIE",
-      rollNumber: "20206045",
-      linkedin: "https://www.linkedin.com/in/shashwat13g/",
-      image: "./images/creators/IMG-20230210-WA0020 - Shashwat Gupta.jpg",
-    }
+  {
+    name: "Alankar Bansal",
+    branch: "ECE",
+    rollNumber: "20205024",
+    linkedin: "#",
+    image: "./images/creators/DSC_6473 - Alankar Bansal.JPG",
+  },
+  {
+    name: "Dolly Agrawal",
+    branch: "Mechanical",
+    rollNumber: "20203053",
+    linkedin: "https://www.linkedin.com/in/dolly-agrawal",
+    image:
+      "./images/creators/WhatsApp Image 2023-02-22 at 11.49.15 PM - Dolly Agrawal.jpeg",
+  },
+  {
+    name: "Anushka Chauhan",
+    branch: "Mechanical",
+    rollNumber: "20203029",
+    linkedin: "https://www.linkedin.com/in/anushka-chauhan-a0b43620a",
+    image: "./images/creators/IMG_20220801_104034_886[1] - Anushka Chauhan.jpg",
+  },
+  {
+    name: "Aitha Sai Rishik",
+    branch: "Electrical",
+    rollNumber: "20202009",
+    linkedin: "#",
+    image: "./images/creators/aitha.jpeg",
+  },
+  {
+    name: "Mohd Taha",
+    branch: "Civil",
+    rollNumber: "20201066",
+    linkedin: "https://www.linkedin.com/in/mohd-taha-mnnit",
+    image: "./images/creators/20230218_212223 - Mohd Taha.jpg",
+  },
+  {
+    name: "Piyush Kashyap",
+    branch: "Civil",
+    rollNumber: "20201075",
+    linkedin: "https://www.linkedin.com/in/piyush-kashyap-a72229203",
+    image: "./images/creators/image - Piyush Kashyap.jpeg",
+  },
+  {
+    name: "Aman Sharma",
+    branch: "Civil",
+    rollNumber: "20201016",
+    linkedin: "https://www.linkedin.com/in/aman-sharma-99a68920a",
+    image: "./images/creators/IMG_20230207_115907_617 - Aman Sharma.jpg",
+  },
+  {
+    name: "Aditya Baranwal",
+    branch: "Civil",
+    rollNumber: "20201010",
+    linkedin: "https://in.linkedin.com/in/aditya-baranwal-013",
+    image: "./images/creators/DSCN1712 - Aditya Baranwal.JPG",
+  },
+  {
+    name: "Ratna Rathaur",
+    branch: "Biotech",
+    rollNumber: "20200041",
+    linkedin: "http://www.linkedin.com/in/ratna-rathaur",
+    image:
+      "./images/creators/IMG_20220602_193643009_PORTRAIT - Ratna Rathaur.jpg",
+  },
+  {
+    name: "Afzal Ansari",
+    branch: "Biotech",
+    rollNumber: "20200007",
+    linkedin: "https://www.linkedin.com/in/afzalansari19",
+    image: "./images/creators/IMG_20230222_002956 - AFZAL ANSARI.jpg",
+  },
+  {
+    name: "Himanshu Gautam",
+    branch: "Chemical",
+    rollNumber: "20209027",
+    linkedin: "#",
+    image: "./images/creators/IMG_20221228_160839_241 - Himanshu gautam.jpg",
+  },
+  {
+    name: "Vishwajeet Kumar",
+    branch: "Chemical",
+    rollNumber: "20209060",
+    linkedin: "https://www.linkedin.com/in/vishwa511",
+    image: "./images/creators/me - vishwajeet kumar.jpg",
+  },
+  {
+    name: "Aman Kumar",
+    branch: "Chemical",
+    rollNumber: "20209004",
+    linkedin: "#",
+    image: "./images/creators/aman kumar.jpeg",
+  },
+  {
+    name: "Adarsh Pandey",
+    branch: "PIE",
+    rollNumber: "20206002",
+    linkedin: "https://www.linkedin.com/in/adarshpandey951",
+    image: "./images/creators/IMG-20230222-WA0000 - Adarsh Pandey.jpg",
+  },
+  {
+    name: "Shashwat Gupta",
+    branch: "PIE",
+    rollNumber: "20206045",
+    linkedin: "https://www.linkedin.com/in/shashwat13g/",
+    image: "./images/creators/IMG-20230210-WA0020 - Shashwat Gupta.jpg",
+  },
+  
 ];
 
 // Get the container to append creator cards
@@ -232,7 +234,6 @@ const creatorsContainer = document.getElementById("container10");
 
 // Get the container to append contributor cards
 const contributorsContainer = document.getElementById("container11");
-
 
 // Function to create creator cards dynamically
 function createCard(creator) {
@@ -255,7 +256,6 @@ function createCard(creator) {
     `;
 }
 
-
 // Loop through the creators and append cards to the container
 creatorsData.forEach((creator) => {
   const creatorCard = createCard(creator);
@@ -267,4 +267,3 @@ contributorsData.forEach((contributor) => {
   const contributorCard = createCard(contributor);
   contributorsContainer.innerHTML += contributorCard;
 });
-
